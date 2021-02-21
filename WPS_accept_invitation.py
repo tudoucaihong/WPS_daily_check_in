de918f8cff84909a1ab1adf8969f6e808477a5b2
tudoucaihong <59330308+tudoucaihong@users.noreply.github.com>
2021-02-21 21:29:42 +0800

    Update WPS_accept_invitation.py

--- WPS_accept_invitation.py
+++ WPS_accept_invitation.py
@@ -18,6 +18,8 @@ sids = [
     "V02SC1mOHS0RiUBxeoA8NTliH2h2NGc00a803c35002693584d"
 ]
 
+
+
 invite_url = 'http://zt.wps.cn/2018/clock_in/api/invite'
 for i in sids:
     requests.post(invite_url, headers={'sid': i}, data={'invite_userid': invite_userid})
